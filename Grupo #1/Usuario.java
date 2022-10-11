55cd992c8e363b2f15b33233881a055f565ae586
Yclunam <64802161+Yclunam@users.noreply.github.com>
2022-10-11 11:33:59 -0500

    Add files via upload

---
+++ Grupo #1/Usuario.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chats;
+
+/**
+ *
+ * @author camil
+ */
+public class Usuario {
+    
+   private String nick;
+
+    public Usuario(String nick) {
+        this.nick = nick;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    @Override
+    public String toString() {
+        return  nick ;
+    }
+
+
+    
+    
+}
