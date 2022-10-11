3a18252095ed1fa9cdb9fb5314dc8119958f5100
mateomorenovelasquez <115569797+mateomorenovelasquez@users.noreply.github.com>
2022-10-11 10:52:46 -0500

    Proyecto
    Se añadió el proyecto al GitHub

---
+++ Grupo #2/Pepe/src/Cliente.java
@@ -0,0 +1,35 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//Conexión TCP
+public class Cliente {
+    public static void main(String[] args)
+    {
+        final String HOST = "127.0.0.1";
+        final int PUERTO = 5000;
+
+        DataInputStream in;
+        DataOutputStream out;
+
+        try
+        {
+            Socket sc = new Socket(HOST, PUERTO);
+
+            in = new DataInputStream(sc.getInputStream());
+            out = new DataOutputStream(sc.getOutputStream());
+
+            out.writeUTF("¡Hola mundo desde el cliente!");
+            String mensaje = in.readUTF();
+
+            System.out.println(mensaje);
+            sc.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
