79841063f786712342c8c0f40faf6d1ec9987120
julsTorres1907 <jutorresf@unal.edu.co>
2022-10-13 16:16:57 -0500

    terminacion del metodo que validael nombre del usuario

--- Grupo #4/TCP_CHAT_ROOM-master/src/main/java/chat/grupo.java
+++ Grupo #4/TCP_CHAT_ROOM-master/src/main/java/chat/grupo.java
@@ -6,8 +6,28 @@ public class grupo {
     static ArrayList<String> grupos;
 
     public String validacionNombre(String nombre){
+        //Le quita los espacios a la cadena
         String nombreSinEspacios = nombre.replaceAll("\\s", "");
-        String nombreMayuscula = nombreSinEspacios.toUpperCase();
-        
+        boolean esValido = true;
+        for(int i=0; i<nombreSinEspacios.length(); i++){
+            //Evalua cada caracter de la cadena y lo pone en mayuscula
+            char caracter = nombreSinEspacios.toUpperCase().charAt(i);
+            //Se obtiene el valor en codigo ASCII de cada caracter
+            int valorASCII = (int)caracter;
+            //Valida si el primer caracter es un numero, si asi es, se sale del ciclo
+            if(i==0 && (valorASCII >= 48 || valorASCII <= 64)){
+                System.out.println("El primer caracter no puede ser un numero");
+                esValido = false;
+                break;
+            }
+            if(i > 0 && (valorASCII > 57 || valorASCII < 65) && valorASCII != 165 && (valorASCII < 48 || valorASCII > 90)){
+                esValido = false;
+            }
+        }
+        if(!esValido){
+            System.out.println("El nombre de usuario NO puede contener caracteres especiales");
+        }
+        String nombreValidado = nombreSinEspacios.toUpperCase();
+        return nombreValidado;
     }
 }
