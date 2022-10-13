d43c28042c9d9782144985755754fd4bbd1f11c0
Francisco Ocampo <fnocampo04@gmail.com>
2022-10-13 09:59:39 -0500

    Se creo la clase grupo y la clase protocolo

---
+++ Grupo #4/TCP_CHAT_ROOM-master/src/main/java/chat/grupo.java
@@ -0,0 +1,8 @@
+package chat;
+
+import java.util.ArrayList;
+
+public class grupo {
+    static ArrayList<String> grupos;
+
+}
---
+++ Grupo #4/TCP_CHAT_ROOM-master/src/main/java/chat/protocolo.java
@@ -0,0 +1,11 @@
+package chat;
+
+public class protocolo {
+    // falta el lector de protocolos, etiqueta comando dato emisor
+    public static void ejecutorprotocolo(String comandoingresado){
+
+        //if (comandoingresado.comando == "mensaje")
+
+
+    }
+}
