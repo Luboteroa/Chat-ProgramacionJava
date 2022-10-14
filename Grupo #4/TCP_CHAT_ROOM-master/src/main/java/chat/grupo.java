8d540d897be1139052defc7c17aad07486cd2f1c
Leandro G <lgarcessa@unal.edu.co>
2022-10-13 21:13:36 -0500

    Restauracion v1

--- Grupo #4/TCP_CHAT_ROOM-master/src/main/java/chat/grupo.java
+++ Grupo #4/TCP_CHAT_ROOM-master/src/main/java/chat/grupo.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 public class grupo {
     static ArrayList<String> grupos;
 
+    /*
     public String validacionNombre(String nombre){
         String nombreSinEspacios = nombre.replaceAll("\\s", "");
         String nombreMayuscula = nombreSinEspacios.toUpperCase();
         
     }
+
+     */
 }
